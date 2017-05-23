ced421eaaa7b932e5faedc9a081aaa4264e13a99
heroazure <1147024669@qq.com>
2017-05-23 22:29:42 +0800

    '添加数据库mysql处理'

--- routes/api/user.js
+++ routes/api/user.js
@@ -7,6 +7,15 @@ var router = express.Router();
 var fs = require('fs')
 var util=require('util')
 var url=require('url')
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : '123456',
+    database : 'hero'
+});
+
+connection.connect();
 
 router.get('/user', function (req, res, next) {
     /*fs.readFile(path.join(__dirname, '../../data/user.json'), 'utf8', function (err, data) {
@@ -32,8 +41,22 @@ router.get('/user/:id', function (req, res, next) {
         res.end(user.name)
     });
 })
-router.post('/user2/test', function (req, res, next) {
-    res.end(util.inspect(req.body))
+router.post('/user', function (req, res, next) {
+    var  addSql = 'INSERT INTO websites(id,name,url,alexa,country) VALUES(0,?,?,?,?)';
+    var body=req.body
+    var  addSqlParams = [body.name, body.url,body.alexa, body.country];
+    connection.query(addSql,addSqlParams,function (err, result) {
+        if(err){
+            console.log('[INSERT ERROR] - ',err.message);
+            return;
+        }
+        console.log('--------------------------INSERT----------------------------');
+        console.log('INSERT:',result);
+        console.log('-----------------------------------------------------------------\n\n');
+    });
+    connection.end();
+
+    res.json({id:addSqlParams})
 })
 
 // export default router
